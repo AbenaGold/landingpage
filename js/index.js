a8d49ec5dfca2e5b0d7eee75e466f2b213c7a144
Priscilla <priscy.o.arthur@gmail.com>
2024-06-10 10:21:28 +0000

    first commit

---
+++ js/index.js
@@ -0,0 +1,9 @@
+const btn = document.getElementById('btn-btn')
+btn.onclick = function(){
+    alert('Wait, more shoes coming soon')
+}
+
+const btn2 = document.getElementById('header-btn')
+btn2.onclick = function(){
+    alert('Sorry, we are out of stock')
+}
